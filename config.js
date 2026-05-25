01b0202c5454c9faa8133e16cac18ca9564774e7
ssmasood78b-oss <ssmasood78b@gmail.com>
2026-05-25 14:36:38 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - Your own Supabase keys
+window.FALAAH_CONFIG = {
+    SUPABASE_URL: 'https://kxbblsqofedwqaixpboo.supabase.co'
+    ANON_KEY: 'sb_publishable_G1PXbwuoxye5E4ZtVGhxVg_Y2wlX0dP'
+}'
